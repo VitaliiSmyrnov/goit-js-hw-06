aea9c9216d0c997120d4aeb800da12648a0ecc50
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-01 14:28:06 +0200

    task 5   додав метод trim

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,8 @@ const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
 function onInputChange(event) {
-  if (event.currentTarget.value !== '') {
-    output.textContent = event.currentTarget.value;
+  if (event.currentTarget.value.trim() !== '') {
+    output.textContent = event.currentTarget.value.trim();
   } else {
     output.textContent = 'Anonymous';
   }
