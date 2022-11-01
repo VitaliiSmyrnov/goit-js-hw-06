3aebc3777787566eb12cfb8713db2db7b24584d3
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-01 21:03:39 +0200

    task 1 виправлено

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@ const categoriesList = document.querySelector('#categories');
 
 console.log(`Number of categories: ${categoriesList.children.length}`);
 
-for (let i = 0; i <= categoriesList.children.length; i++) {
+for (let i = 0; i < categoriesList.children.length; i++) {
   console.log(`Category: ${categoriesList.children[i].firstElementChild.textContent}
 Elements: ${categoriesList.children[i].lastElementChild.children.length}`);
 }
